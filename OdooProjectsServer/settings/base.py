eacd824f1c54ddae301a8aef5f27fbc73e5a6d6a
Maks <maks.ismoilov0618@gmail.com>
2019-06-10 17:43:35 +0300

    preparing to deploying to aws ubuntu virtual machine

--- OdooProjectsServer/settings/base.py
+++ OdooProjectsServer/settings/base.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'OdooProjectsServer.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, '../../templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'OdooProjectsServer.wsgi.application'
 DATABASES = {
      'default': {
          'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+         'NAME': os.path.join(BASE_DIR, '../../db.sqlite3'),
      }
 }
 
