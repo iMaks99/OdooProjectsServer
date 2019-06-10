06d14faafeee7a371cabd732b92d2d3f5e0e7931
Maks <maks.ismoilov0618@gmail.com>
2019-06-10 23:37:15 +0300

    added postgresql

--- OdooProjectsServer/settings/base.py
+++ OdooProjectsServer/settings/base.py
@@ -77,8 +77,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'odooserver',
-        'USER': '',
-        'PASSWORD': '',
+        'USER': 'postgres',
+        'PASSWORD': 'odooprojects',
         'HOST': 'localhost',
         'PORT': '5432',
     }
